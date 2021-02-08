0252fffb5fe3a9e0d634fe6844d9e9dbb0497e38
alexwoodsy <61010135+alexwoodsy@users.noreply.github.com>
2021-02-08 18:22:22 +0000

    chnage the nuild path of the fron end

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const server = Server({
 // };
 
  // Build path relative to the server.js file
- const frontEndAppBuildPath = path.resolve(__dirname, './build');
+ const frontEndAppBuildPath = path.resolve(__dirname, '../build');
  server.app.use(serve(frontEndAppBuildPath))
  
  server.run(PORT, () => {
